0fbabe786581919dc07a4ac822fa5f70f6c4cacf
shop-kins <silver7392@hotmail.co.uk>
2023-10-29 23:36:53 +0000

    Some slight chef changes

--- chef/cookbooks/initial_install/recipes/default.rb
+++ chef/cookbooks/initial_install/recipes/default.rb
@@ -8,20 +8,20 @@ end
 execute "docker_start" do
   command "sudo systemctl start docker"
   action :nothing
-  notifies :run, 'execute[docker_compose_download]', :delayed
+  notifies :run, 'execute[docker_compose_download]', :immediate
 end
 
 execute "docker_install" do
   command "sudo yum install -y docker"
   action :run
   not_if 'command -v docker'
-  notifies :run, 'execute[docker_start]', :delayed
+  notifies :run, 'execute[docker_start]', :immediate
 end
 
 execute "docker_compose_download" do
   command "curl -L https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-compose-plugin-2.6.0-3.el7.x86_64.rpm -o ./compose-plugin.rpm"
   action :nothing
-  notifies :run, 'execute[docker_compose_install]', :delayed
+  notifies :run, 'execute[docker_compose_install]', :immediate
 end
 
 execute "docker_compose_install" do
